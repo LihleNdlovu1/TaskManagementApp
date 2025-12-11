8f51ab6db5693e3cf9d4806dafecdea4937d923b
Liehle <lubelihlendlovu199@gmail.com>
2025-12-11 09:48:30 +0200

    Initial commit for Task Management App

--- Task Management app/ViewController.swift
+++ Task Management app/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+struct Task {
+    var title: String
+    var isCompleted: Bool
+}
+
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks: [Task] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+   
+    @IBAction func addTaskTapped(_ sender: UIButton) {
+    guard let text = taskTextField.text, !text.isEmpty else {
+            print("Text field is empty")
+            return
+        }
+        
+        let newTask = Task(title: text, isCompleted: false)
+        tasks.append(newTask)
+        print("Task added: \(newTask.title)")
+        
+        taskTextField.text = ""
+        tableView.reloadData()
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell") ??
+                   UITableViewCell(style: .default, reuseIdentifier: "TaskCell")
+
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        cell.accessoryType = task.isCompleted ? .checkmark : .none
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tasks[indexPath.row].isCompleted.toggle()
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
 }
-
